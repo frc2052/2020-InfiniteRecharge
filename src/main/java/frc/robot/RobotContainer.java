ad0d0fcea87c1a16b851d1f16f1d88278a3e931b
lloydhilsgen001 <2020hilsl93@moundsviewschools.org>
2020-01-18 13:42:30 -0600

    created AutoPathGetter.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,9 +9,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ArmToggle;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.OuterIntakeIn;
+import frc.robot.commands.*;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
