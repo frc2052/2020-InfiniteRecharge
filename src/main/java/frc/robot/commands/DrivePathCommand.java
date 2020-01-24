49bff6709156105a2747b968c77d860a917bf394
Katie <2021SjobK75@moundsviewschools.org>
2020-01-24 15:09:04 -0600

    drive path command now gets a path from an enum rather than auto selector

--- src/main/java/frc/robot/commands/DrivePathCommand.java
+++ src/main/java/frc/robot/commands/DrivePathCommand.java
@@ -8,23 +8,29 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.auto.AutoPathGetter;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import java.util.List;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import frc.robot.Constants;
 
+
 public class DrivePathCommand extends CommandBase {
- 
   private final DriveTrainSubsystem m_driveTrainSubsystem;
+  private final DrivePathEnum m_choosenPath;
 
-  public DrivePathCommand(DriveTrainSubsystem driveTrain) {
+  public DrivePathCommand(DriveTrainSubsystem driveTrain, DrivePathEnum pathEnum) {
     m_driveTrainSubsystem = driveTrain;
-
-    addRequirements(driveTrain);
-    // Use addRequirements() here to declare subsystem dependencies.
+    m_choosenPath = pathEnum;
   }
 
   // Called when the command is initially scheduled.
@@ -32,26 +38,79 @@ public class DrivePathCommand extends CommandBase {
   public void initialize() {
   }
 
+  public Trajectory getTrajectory(DrivePathEnum selectedPath) {
+      var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
+              new SimpleMotorFeedforward(Constants.DriveTrain.ksVolts, Constants.DriveTrain.kvVoltSecondsPerMeter,
+                      Constants.DriveTrain.kaVoltSecondsSquaredPerMeter),
+              Constants.DriveTrain.kinematics, 10);
+
+      TrajectoryConfig config = new TrajectoryConfig(Constants.Autonomous.maxVelocity,
+              Constants.Autonomous.maxAcceleration).setKinematics(Constants.DriveTrain.kinematics)
+                      .addConstraint(autoVoltageConstraint);
+
+      switch(selectedPath)  {
+        case StartCenterDriveBackPark:
+            Trajectory CS = TrajectoryGenerator.generateTrajectory(new Pose2d(3.048, 0, new Rotation2d(0)),
+              List.of(new Translation2d(3.048, 0)), new Pose2d(3.81, 0, new Rotation2d(0)), config); 
+            return CS;
+        case StartCenterGenerator3:
+            Trajectory CSG3 = TrajectoryGenerator.generateTrajectory(new Pose2d(3.048, 0, new Rotation2d(0)),
+              List.of(new Translation2d(5.2324, -2.0066), new Translation2d(6.1468, -2.0066)),
+            new Pose2d(5.3086, -1.397, new Rotation2d(0)), config);
+          return CSG3;
+        case StartLeftGenerator3:
+            Trajectory LSG3 = TrajectoryGenerator.generateTrajectory(new Pose2d(3.048, -1.7018, new Rotation2d(0)),
+              List.of(new Translation2d(5.2324, -2.0066), new Translation2d(6.1468, -2.0066)),
+                new Pose2d(5.3086, -1.397, new Rotation2d(0)), config);
+            return LSG3;
+        case StartLeftGenerator5:
+            Trajectory LSG5 = TrajectoryGenerator.generateTrajectory(new Pose2d(3.048, -1.7018, new Rotation2d(0)),
+              List.of(new Translation2d(5.2324, -2.0066), new Translation2d(6.1468, -2.0066),
+                new Translation2d(5.3086, -1.397), new Translation2d(5.7658, -1.0922),
+                new Translation2d(6.223, -0.7874)),
+              new Pose2d(6.5278, -0.508, new Rotation2d(0)), config);
+            return LSG5;
+        case StartLeftTrench2:
+            Trajectory LST2 = TrajectoryGenerator.generateTrajectory(new Pose2d(3.048, -1.7018, new Rotation2d(0)),
+              List.of(new Translation2d(6.35, -5.334), new Translation2d(6.5024, -5.08)),
+            new Pose2d(6.35, -4.8514, new Rotation2d(0)), config);
+          return LST2;
+        case StartRightTrench3Ball:
+            Trajectory RST3 = TrajectoryGenerator.generateTrajectory(new Pose2d(3.048, -1.7272, new Rotation2d(0)),
+              List.of(new Translation2d(6.1722, -1.7272)), new Pose2d(8.001, -1.7272, new Rotation2d(0)), config);
+          return RST3;
+        case LeftTrenchToMiddle: //TODO: fix these values
+            Trajectory LTM = TrajectoryGenerator.generateTrajectory(new Pose2d(3.048, -1.7272, new Rotation2d(0)),
+              List.of(new Translation2d(6.1722, -1.7272)), new Pose2d(8.001, -1.7272, new Rotation2d(0)), config);
+          return LTM;
+        default:
+            Trajectory DontMove = TrajectoryGenerator.generateTrajectory(new Pose2d(3.048, 0, new Rotation2d(0)),
+              List.of(new Translation2d(3.048, 0)), new Pose2d(3.81, 0, new Rotation2d(0)), config);
+          return DontMove;
+      }
+  }
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     RamseteCommand ramseteCommand = new RamseteCommand(
-        AutoPathGetter.getSelectedTrajectory(),
-        m_driveTrainSubsystem::getPose,
-        new RamseteController(Constants.DriveTrain.kRamseteB, Constants.DriveTrain.kRamseteZeta),
-        new SimpleMotorFeedforward(Constants.DriveTrain.ksVolts,
-                                   Constants.DriveTrain.kvVoltSecondsPerMeter,
-                                   Constants.DriveTrain.kaVoltSecondsSquaredPerMeter),
-        Constants.DriveTrain.kinematics,
-        m_driveTrainSubsystem::getWheelSpeeds,
-        new PIDController(Constants.DriveTrain.kPDriveVel, 0, 0),
-        new PIDController(Constants.DriveTrain.kPDriveVel, 0, 0),
-        // RamseteCommand passes volts to the callback
-        m_driveTrainSubsystem::tankDriveVolts,
-        m_driveTrainSubsystem
-    );
+      getTrajectory(m_choosenPath),
+      m_driveTrainSubsystem::getPose,
+      new RamseteController(Constants.DriveTrain.kRamseteB, Constants.DriveTrain.kRamseteZeta),
+      new SimpleMotorFeedforward(Constants.DriveTrain.ksVolts,
+                                 Constants.DriveTrain.kvVoltSecondsPerMeter,
+                                 Constants.DriveTrain.kaVoltSecondsSquaredPerMeter),
+      Constants.DriveTrain.kinematics,
+      m_driveTrainSubsystem::getWheelSpeeds,
+      new PIDController(Constants.DriveTrain.kPDriveVel, 0, 0),
+      new PIDController(Constants.DriveTrain.kPDriveVel, 0, 0),
+      // RamseteCommand passes volts to the callback
+      m_driveTrainSubsystem::tankDriveVolts,
+      m_driveTrainSubsystem
+  );
 
-    ramseteCommand.andThen(() -> m_driveTrainSubsystem.tankDriveVolts(0, 0));
+  ramseteCommand.andThen(() -> m_driveTrainSubsystem.tankDriveVolts(0, 0));
+
   }
 
   // Called once the command ends or is interrupted.
@@ -68,6 +127,7 @@ public class DrivePathCommand extends CommandBase {
   public enum DrivePathEnum
   {
     StartCenterDriveBackPark,
+    StartCenterGenerator3,
     StartRightTrench3Ball,
     StartLeftGenerator3,
     StartLeftTrench2,
