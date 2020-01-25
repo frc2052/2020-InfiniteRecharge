de893c08a3c186851b081b0cfb2872c4e59fd8cd
wafflewhal3 <quruxland@gmail.com>
2020-01-24 20:18:44 -0600

    Initial conveyor work

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -90,4 +90,11 @@ public final class Constants {
         public static final int targetHeight = 249; //cm
         public static final int mountingAngle = 0;
     }
+
+    public static final class ConveyorSubsystem {      
+        public static final int kConveyorMotorBottemLeftID = 0;   
+        public static final int kConveyorMotorBottemRightID = 0;   
+        public static final int kLifterFrontID = 0;   
+        public static final int kLifterBackID = 0;
+    }
 }
---
+++ src/main/java/frc/robot/commands/ConveyorUpCommand.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ConveyorSubsystem;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class ConveyorUpCommand extends CommandBase {
+  private final ConveyorSubsystem m_subsystem;
+
+  /**
+   * Creates a new ConveyorUpCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ConveyorUpCommand(ConveyorSubsystem subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+subsystem.LifterUp();
+    addRequirements(subsystem);
+  } 
+}
