546655c80cb2fd6d91af83aaa4969955220c72e6
Katie <katie.sj445@gmail.com>
2020-02-05 19:28:19 -0600

    Updated drive path command so it will actually run a ramsete command

--- src/main/java/frc/robot/auto/StartLeftGenerator3Command.java
+++ src/main/java/frc/robot/auto/StartLeftGenerator3Command.java
@@ -10,22 +10,20 @@ package frc.robot.auto;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import frc.robot.commands.*;
-import frc.robot.commands.DrivePathCommand.DrivePathEnum;
+import frc.robot.auto.TrajectoryFactory.DrivePathEnum;
 import frc.robot.subsystems.*;
 
 public class StartLeftGenerator3Command extends SequentialCommandGroup {
-  /**
-   * Creates a new StartLeftGenerator3Command.
-   */
+  public TrajectoryFactory trajectoryFactory = new TrajectoryFactory();
 
   public StartLeftGenerator3Command(DriveTrainSubsystem driveTrain, ShooterSubsystem shooter, IntakeSubsystem intake, VisionSubsystem vision, HoodSubsystem hood, TurretSubsystem turret, ConveyorSubsystem conveyor, Double delayTime) {
 	    this.addCommands(new WaitCommand(delayTime));
       this.addCommands(new AutoShooterCommand(shooter, vision, hood, turret, conveyor));
       ArmDownCommand intakeCmd = new ArmDownCommand(intake);
-      DrivePathCommand path1 = new DrivePathCommand(driveTrain, DrivePathEnum.StartLeftGenerator3);
-      ParallelCommandGroup par1 = new ParallelCommandGroup(intakeCmd, path1);
-      
+      RamseteCommand ramsete = trajectoryFactory.getRamseteCommand(driveTrain, DrivePathEnum.StartLeftGenerator3);
+      ParallelCommandGroup par1 = new ParallelCommandGroup(intakeCmd, ramsete);
       this.addCommands(par1);
       this.addCommands(new OuterIntakeStopCommand(intake));
       this.addCommands(new AutoShooterCommand(shooter, vision, hood, turret, conveyor));
--- src/main/java/frc/robot/auto/StartLeftShoot5Command.java
+++ src/main/java/frc/robot/auto/StartLeftShoot5Command.java
@@ -8,46 +8,26 @@
 package frc.robot.auto;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.*;
-import frc.robot.commands.DrivePathCommand.DrivePathEnum;
+import frc.robot.auto.TrajectoryFactory.DrivePathEnum;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class StartLeftShoot5Command extends SequentialCommandGroup {
-  /**
-   * Creates a new AutoLeftShoot5.
-   */
+  public TrajectoryFactory trajectoryFactory = new TrajectoryFactory();
+
   public StartLeftShoot5Command(DriveTrainSubsystem driveTrain, ShooterSubsystem shooter, IntakeSubsystem intake, VisionSubsystem vision, HoodSubsystem hood, TurretSubsystem turret, ConveyorSubsystem conveyor, Double delayTime) {
     this.addCommands(new WaitCommand(delayTime));
     this.addCommands(new AutoShooterCommand(shooter, vision, hood, turret, conveyor));
     ArmDownCommand intakeCmd = new ArmDownCommand(intake);
-    DrivePathCommand path1 = new DrivePathCommand(driveTrain, DrivePathEnum.CenterGenerator5);
+    RamseteCommand ramsete = trajectoryFactory.getRamseteCommand(driveTrain, DrivePathEnum.StartLeftGenerator5);
     ArmDownCommand armDownCmd = new ArmDownCommand(intake);
-    ParallelCommandGroup par1 = new ParallelCommandGroup(intakeCmd, path1, armDownCmd);
+    ParallelCommandGroup par1 = new ParallelCommandGroup(intakeCmd, ramsete, armDownCmd);
     this.addCommands(par1);
     this.addCommands(new OuterIntakeStopCommand(intake));
     this.addCommands(new AutoShooterCommand(shooter, vision, hood, turret, conveyor));
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
 }
