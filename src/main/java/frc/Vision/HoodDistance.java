aa9d24c611a9c2347b9e8440e192401ec5acd180
Caleb Hill <2024hillc03@moundsviewschools.org>
2020-02-22 11:00:31 -0600

    Added classes

---
+++ src/main/java/frc/Vision/HoodDistance.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.Vision;
+
+/**
+ * Add your docs here.
+ */
+public class HoodDistance {
+    private int m_Inches;
+    private int m_HoodTicks;
+    public HoodDistance(int inches, int hoodTicks){
+        m_Inches = inches;
+        m_HoodTicks = hoodTicks;
+    }
+    public int getInches(){
+        return m_Inches;
+    }
+    public int getHoodTicks(){
+        return m_HoodTicks;
+    }
+}
---
+++ src/main/java/frc/Vision/VisionCalculator.java
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.Vision;
+
+import java.util.ArrayList;
+
+/**
+ * Add your docs here.
+ */
+public class VisionCalculator {
+    private ArrayList<Integer> position = new ArrayList<Integer> ();//in inches from goal
+    private ArrayList<Integer> hoodTicks = new ArrayList<Integer> ();//to score a goal
+    
+}
