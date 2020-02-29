1c2880f575926278e0bab2bed94c4464926ccd8b
Chris Hardwick <2021hardc99@Moundsviewschools.org>
2020-02-29 16:45:33 -0600

    made a new start left trench two auto and a new path

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -80,7 +80,7 @@ public class Robot extends TimedRobot {
 
   /**
    * This function is called once each time the robot enters Disabled mode.
-   */
+   */ 
   @Override
   public void disabledInit() {
     CommandScheduler.getInstance().cancelAll();
--- src/main/java/frc/robot/auto/StartLeftTrench2Command.java
+++ src/main/java/frc/robot/auto/StartLeftTrench2Command.java
@@ -9,6 +9,7 @@ package frc.robot.auto;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.*;
@@ -26,13 +27,20 @@ public class StartLeftTrench2Command extends SequentialCommandGroup {
   public StartLeftTrench2Command(DriveTrainSubsystem driveTrain, ShooterSubsystem shooter, IntakeSubsystem intake, VisionSubsystem vision, HoodSubsystem hood, TurretSubsystem turret, ConveyorSubsystem conveyor, Double delayTime, AutoShooterControls controls) {
       this.addCommands(new BumpCommand(driveTrain));
       this.addCommands(new WaitCommand(delayTime));
-      this.addCommands(new AutoShooterCommand(shooter, vision, hood, turret, conveyor, controls, Constants.Autonomous.kLeftInitTargetTicks, 3));
+      this.addCommands(new AutoControlsCommand(controls, false, true));
+      Command driveToMidTrench = trajectoryFactory.getRamseteCommand(driveTrain, DrivePathEnum.StartLeftTrench2);
       ArmDownCommand intakeCmd = new ArmDownCommand(intake);
-      Command ramsete = trajectoryFactory.getRamseteCommand(driveTrain, DrivePathEnum.StartLeftTrench2);
-      ParallelCommandGroup par1 = new ParallelCommandGroup(intakeCmd, ramsete);
-      this.addCommands(par1);
-      this.addCommands(new OuterIntakeStopCommand(intake));
-      this.addCommands(trajectoryFactory.getRamseteCommand(driveTrain, DrivePathEnum.LeftTrenchToMiddle));
+      ParallelCommandGroup intakeDrive2Balls = new ParallelCommandGroup(intakeCmd, driveToMidTrench);
+      this.addCommands(intakeDrive2Balls);
+
+      AutoReadyCommand ready = new AutoReadyCommand(shooter, vision, hood, turret, conveyor, controls, 0);
+      Command driveToShootSpot = trajectoryFactory.getRamseteCommand(driveTrain, DrivePathEnum.Trench2ToShoot);
+      ParallelDeadlineGroup driveReady = new ParallelDeadlineGroup(driveToShootSpot, ready);
+      this.addCommands(driveReady);
+      this.addCommands(new AutoControlsCommand(controls, true, true));
       this.addCommands(new AutoShooterCommand(shooter, vision, hood, turret, conveyor, controls, 0, 5));
+      this.addCommands(new ArmUpCommand(intake));
+      this.addCommands(new OuterIntakeStopCommand(intake));
+      
   }
 }
